c9df81ecf9a370a1724eff0c4618b1a43c307b5f
Omer Aplatony <61663422+omerap12@users.noreply.github.com>
2021-11-02 15:02:51 +0200

    Add files via upload

---
+++ deepestLeavesSum.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def deepestLeavesSum(self, root):
+        deepest = self.halper(root)
+        return self.answer(root, 0, deepest)
+
+    def halper(self, root):
+        if root is None:
+            return 0
+        return max(self.halper(root.left) + 1, self.halper(root.right) + 1)
+
+    def answer(self, root, current, maxDepth):
+        if root is None:
+            return 0
+        if root.left is None and root.right is None and current == maxDepth:
+            return root.val
+        return self.answer(root.left, current + 1, maxDepth) + self.answer(root.right, current + 1, maxDepth)
+
+
