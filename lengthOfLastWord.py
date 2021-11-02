24b4d8acd6af5553db253fc93bc3dc16ce4b0f0c
Omer Aplatony <61663422+omerap12@users.noreply.github.com>
2021-11-02 15:04:08 +0200

    Create lengthOfLastWord.py

---
+++ lengthOfLastWord.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        word = s.rstrip()
+        count = 0
+        if s == "":
+            return count
+
+        for i in range(len(word) - 1, -1, -1):
+            if word[i] == " ":
+                return count
+            count += 1
+        return count
