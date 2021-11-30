f0b65813e9dcc25eb7f2c311f21e162c1a08973a
Omer Aplatony <61663422+omerap12@users.noreply.github.com>
2021-11-30 22:03:39 +0200

    Create groupThePeople.py

---
+++ groupThePeople.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def groupThePeople(self, groupSizes):
+        answer = []
+        data_table = {} # value : [all indexes with the value]
+        # iterating through the list, and entering data to the dataTable
+        for i in range(len(groupSizes)):
+            if groupSizes[i] not in data_table.keys():
+                data_table[groupSizes[i]] = [i]
+            else:
+                data_table[groupSizes[i]].append(i)
+        # iterating through the data table and cut the list according to key value
+        for key, value in data_table.items():
+            start_index = 0
+            stop_index = len(value)
+            # cutting lists
+            for i in range(start_index, stop_index, key):
+                start_index = i
+                answer.append(value[start_index:start_index+key])
+        return answer
