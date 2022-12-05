2d1d084005e2ec49b3899680f4e19788023d841e
Omer <61663422+omerap12@users.noreply.github.com>
2022-12-05 11:30:52 +0200

    Fixed wrong solution

--- python3/Top_Interview_Question_Medium/product_of_array_except_self.py
+++ python3/Top_Interview_Question_Medium/product_of_array_except_self.py
@@ -1,25 +1,23 @@
 # https://leetcode.com/problems/product-of-array-except-self/
 
-class Solution(object):
+class Solution:
     def productExceptSelf(self, nums):
-        product = 1
-        zero_index = set()
-        flag = False
+        prefix_product = []
+        suffix_product = [0]*len(nums)
+        current_product = 1
         for i in range(len(nums)):
-            if nums[i] == 0:
-                flag = True
-                zero_index.add(i)
-            else:
-                product = product*nums[i]
-        answer = []
+            current_product *= nums[i]
+            prefix_product.append(current_product)
+        current_product = 1
+        for j in range(len(nums) - 1, -1,-1):
+            current_product *= nums[j]
+            suffix_product[j] = current_product
+        answer = [0]*len(nums)
         for i in range(len(nums)):
-            if flag and i not in zero_index:
-                answer.append(0)
-            elif flag and i in zero_index:
-                if len(zero_index) == 1:
-                    answer.append(product)
-                else:
-                    answer.append(0)
+            if 0 < i < len(nums)-1:
+                answer[i] = prefix_product[i-1]*suffix_product[i+1]
+            elif i == 0:
+                answer[i] = suffix_product[i+1]
             else:
-                answer.append(int(product/nums[i]))
+                answer[i] = prefix_product[len(nums)-2]
         return answer
