ccba8fee54a2c88682149358f1fdffa2d9ea9e49
Omer <omerap12.gmail.com>
2023-10-09 14:03:04 +0300

    Solved Reverse Words in a String

--- python3/Leetcode75/ReverseVowelsofaString.py
+++ python3/Leetcode75/ReverseVowelsofaString.py
@@ -1,25 +1,6 @@
-# https://leetcode.com/problems/reverse-vowels-of-a-string/description/?envType=study-plan-v2&envId=leetcode-75
+# https://leetcode.com/problems/reverse-words-in-a-string/description/?envType=study-plan-v2&envId=leetcode-75
 class Solution:
-    def reverseVowels(self, s: str) -> str:
-        ret = ""
-        index_to_vowles = {}
-        index = 0
-        vowles = ('e','E','a','A','i','I','o','O','u','U')
-        for word in s:
-            if word in vowles:
-                index_to_vowles[index] = word
-                index += 1
-        
-
-        curr = index - 1
-        for i in range(len(s)):
-            if s[i] not in vowles:
-                ret += s[i]
-            else:
-                ret += index_to_vowles[curr]
-                curr -= 1 
+    def reverseWords(self, s: str) -> str:
+        words_splitted = s.split(' ')
+        ret = " ".join(word for word in words_splitted[::-1] if word != "" and word != " ")
         return ret
-
-
-
-        
