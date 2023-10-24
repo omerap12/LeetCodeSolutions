cb1af1ee519036d5fa4241454206649610ab77f4
Omer <omerap12.gmail.com>
2023-10-24 16:05:29 +0300

    Solved Reorder list problem

---
+++ python3/LeetcodeTopInterview150/ReorderList.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/reorder-list/description/
+
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+       stack = []
+       tmp_head = head.next
+       while tmp_head:
+           stack.append(tmp_head)
+           tmp_head = tmp_head
+           tmp_head = tmp_head.next
+       
+       temp = head
+       while stack:
+           temp.next = stack.pop()
+           temp = temp.next
+           if stack:
+            temp.next = stack.pop(0)
+            temp = temp.next
+       temp.next = None
+       return head
